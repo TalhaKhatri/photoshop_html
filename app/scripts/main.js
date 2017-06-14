339a1fb86cee4196b5415c331e4ed9d63553840d
Talha <talha@marketlytics.com>
2017-06-14 17:24:36 +0500

    Initiated project and created some elements

---
+++ app/scripts/main.js
@@ -0,0 +1,3 @@
+import css from '../styles/main.css';
+import html from '../index.html';
+console.log('\'Allo \'Allo!');
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: './app/scripts/main.js',
+  output: {
+    filename: './app/scripts/bundle.js'
+  },
+  module: {
+      loaders: [
+        { test: /\.css$/, loader: "style-loader!css-loader" },
+        { test: /\.png$/, loader: "url-loader?mimetype=image/png" },
+        { test: /\.html$/, use: 'raw-loader'}
+      ],
+    },
+  watch: true
+};
